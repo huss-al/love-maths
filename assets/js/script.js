19fc66ec5507deb349a8b9c5920c535d238ca343
Huss Al Dafai <h_aldafai@hotmail.com>
2024-05-16 11:19:46 +0000

    added favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,14 @@ document.addEventListener("DOMContentLoaded", function() {
          })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer()
+        }
+    })
+
     runGame("addition");
-    runGame("multiply");
-    runGame("subtract");
+
 
 })
 
@@ -24,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed. 
  */
 function runGame (gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.ceil(Math.random() * 25)
     let num2 = Math.ceil(Math.random() * 25)
 
