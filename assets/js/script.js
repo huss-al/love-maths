3177d1f227df1f1eff2d90a81a1f8fcefa00604f
Huss Al Dafai <h_aldafai@hotmail.com>
2024-05-16 10:31:24 +0000

    added multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     runGame("addition");
+    runGame("multiply")
 
 })
 
@@ -27,10 +28,13 @@ function runGame (gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2)
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2)
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 
@@ -68,10 +72,12 @@ function calculateCorrectAnswer () {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
-    }
+    };
 
 }
 
@@ -105,7 +111,9 @@ function displayAdditionQuestion (operand1, operand2) {
 
  }
 
- function displayMultiplyQuestion () {
-
+ function displayMultiplyQuestion (operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
  }
   
